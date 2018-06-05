672ef861cbf81a27fbf616b6f0da13ceaf74a5df
Juan Montoya <jcmontoya@gmail.com>
2018-06-05 20:24:30 +0000

    section 1 done

--- MEAN/app.js
+++ MEAN/app.js
@@ -1,13 +1,25 @@
-require('./instantHello');
-var goodbye = require('./talk/goodbye');
-var talk = require('./talk');
-var question = require('./talk/question');
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var routes = require('./api/routes');
+
+app.set('port', process.env.PORT);
+
+app.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded( { extended : false } ));
+
+app.use('/api', routes);
 
 
-talk.intro();
-talk.hello("Juan");
-
-var answer = question.ask("What is the meaning of life?");
-console.log(answer);
-
-goodbye();
+var server = app.listen(app.get('port'), function() {
+    var port = server.address().port;
+    console.log("The magic happens on port " + port);
+});
